7ffec23e62b8d81dda8dcba401c4b0d2cb8b8071
Gniox <gniox15@gmail.com>
2020-09-23 17:25:37 -0500

    flip animation dependency

--- src/TaskLoader.js
+++ src/TaskLoader.js
@@ -1,35 +1,21 @@
 import React from "react";
 import "./index.css";
+import FlipMove from "react-flip-move";
 class TaskLoader extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      color: "#e3fac5"
-    };
 
     this.createTasks = this.createTasks.bind(this);
-    this.changeColor = this.changeColor.bind(this);
-    this.changeColorBack = this.changeColorBack.bind(this);
   }
 
   delete(key) {
     this.props.delete(key);
   }
 
-  changeColor() {
-    this.setState({ color: "#eb5e8d" });
-  }
-
-  changeColorBack() {
-    this.setState({ color: "#e3fac5" });
-  }
-
   createTasks(item, style) {
     return (
       <li
         onClick={() => this.delete(item.key)}
-        onMouseEnter={this.changeColor}
-        onMouseLeave={this.changeColorBack}
         key={item.key}
         style={style}
         className="task"
@@ -51,15 +37,7 @@ class TaskLoader extends React.Component {
       paddingLeft: 0
     };
     let liStyle = {
-      listStyle: "none",
-      backgroundColor: this.state.color,
-      borderRadius: 20,
-      textAlign: "center",
-      marginBottom: "4%",
-      marginTop: "4%",
-      fontFamily: "sans-serif",
-      paddingTop: "2%",
-      paddingBottom: "2%",
+      
       fontSize: this.props.smaller ? 15 : 24
     };
     console.log("this is props.tasks: " + this.props.tasks);
@@ -72,7 +50,13 @@ class TaskLoader extends React.Component {
       listTasks.push(this.createTasks(task, liStyle));
     }
 
-    return <ul style={containerStyle}>{listTasks}</ul>;
+    return (
+      <ul style={containerStyle}>
+        <FlipMove duration={250} easing="ease-out">
+          {listTasks}
+        </FlipMove>
+      </ul>
+    );
   }
 }
 
