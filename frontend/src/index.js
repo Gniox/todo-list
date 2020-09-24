de8cfdf247ea5856f8531e6eca08479d0a465662
Gniox <gniox15@gmail.com>
2020-09-24 09:22:52 -0500

    changed todolist to container

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ToDoList from './ToDoList.js';
+import Container from './Container';
 
 var destination = document.querySelector("#root");
 
 ReactDOM.render(
-    <ToDoList></ToDoList>,
+    <Container></Container>,
   destination
 );
