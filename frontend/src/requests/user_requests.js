53aa7f6a3bec059876de0d8928d14e7967041af1
Chaly Xiong <gniox15@gmail.com>
2020-10-04 06:40:20 -0500

    got rid of some print statements

--- frontend/src/requests/user_requests.js
+++ frontend/src/requests/user_requests.js
@@ -1,5 +1,4 @@
 async function sendLogIn(email, password) {
-  console.log("anything ehre?");
   fetch("http://localhost:4000/user/login", {
     method: "POST",
     body: JSON.stringify({
@@ -12,8 +11,6 @@ async function sendLogIn(email, password) {
   })
     .then(function (response) {
       if (response.ok) {
-        // let token = response.token;
-        // let user = this.getUser(response.json(response.json().token));
         return response.json();
       }
       return Promise.reject(response);
@@ -28,7 +25,6 @@ async function sendLogIn(email, password) {
 }
 
 async function getUser() {
-  console.log("12345");
   fetch("http://localhost:4000/user/me", {
     method: "GET",
     headers: {
@@ -44,8 +40,6 @@ async function getUser() {
     })
     .then(async (data) => {
       await localStorage.setItem("@user", JSON.stringify(data));
-      console.log("should have set local storage to user");
-      //  localStorage.setItem("@user", data);
     })
     .catch(function (error) {
       console.warn("something went wrong.", error);
